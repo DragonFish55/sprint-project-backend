04b0d2e53f2d9ba611e47fa4a05266f2600e9cd7
DragonFish55 <61723984+DragonFish55@users.noreply.github.com>
2022-02-09 22:44:14 -0500

    Updated database

--- app.py
+++ app.py
@@ -17,7 +17,8 @@ heroku=Heroku()
 #migrate=Migrate()
 db.init_app(app)
 #migrate.init_app(app,db)
-
+with app.app_context:
+    db.create_all()
 
 #create local account
 @app.route('/signup', methods = ["POST"])
