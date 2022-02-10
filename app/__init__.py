23b13124289a54d4e072f2592c25cb1b9ecf309d
DragonFish55 <61723984+DragonFish55@users.noreply.github.com>
2022-02-10 11:33:34 -0500

    Updated files

--- app/__init__.py
+++ app/__init__.py
@@ -9,7 +9,7 @@ db=SQLAlchemy()
 app=Flask(__name__)
 CORS(app)
 app.config['CORS_HEADERS']='Content-Type'
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://ronexffqrugnjx:5ab471aedc41d2a7a1f8da2a0cc912c5210dfe9ff83e2d3fb71bf4fa0692f17d@ec2-34-194-171-47.compute-1.amazonaws.com:5432/d77is03fnt6nrs"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://ronexffqrugnjx:5ab471aedc41d2a7a1f8da2a0cc912c5210dfe9ff83e2d3fb71bf4fa0692f17d@ec2-34-194-171-47.compute-1.amazonaws.com:5432/d77is03fnt6nrs"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = "secret"
 heroku=Heroku(app)
