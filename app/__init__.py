ec37aed67f00002625d2f9baef9d1a763e5fab75
DragonFish55 <61723984+DragonFish55@users.noreply.github.com>
2022-02-10 18:00:23 -0500

    Updated files

--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,7 @@ app=Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql+psycopg2://ronexffqrugnjx:5ab471aedc41d2a7a1f8da2a0cc912c5210dfe9ff83e2d3fb71bf4fa0692f17d@ec2-34-194-171-47.compute-1.amazonaws.com:5432/d77is03fnt6nrs"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['CORS_HEADERS']='Content-Type'
-CORS(app)
+cors = CORS(app)
 db=SQLAlchemy(app)
 migrate = Migrate(app,db)
 app.secret_key = "secret"
