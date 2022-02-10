c4c79959e292cc0c850025a15ff777c277de53b5
DragonFish55 <61723984+DragonFish55@users.noreply.github.com>
2022-02-10 16:25:59 -0500

    Fixed heroku errors

--- app/__init__.py
+++ app/__init__.py
@@ -5,12 +5,6 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
-
-
-
-
-
-
 app=Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql+psycopg2://ronexffqrugnjx:5ab471aedc41d2a7a1f8da2a0cc912c5210dfe9ff83e2d3fb71bf4fa0692f17d@ec2-34-194-171-47.compute-1.amazonaws.com:5432/d77is03fnt6nrs"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -21,11 +15,6 @@ migrate = Migrate(app,db)
 app.secret_key = "secret"
 heroku=Heroku(app)
 db.init_app(app)
-
-
-
-
-
 
 with app.app_context():
     db.create_all()
