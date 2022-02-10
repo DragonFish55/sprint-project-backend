64cf07ef261ee031e627f7778a916197c57bfa90
DragonFish55 <61723984+DragonFish55@users.noreply.github.com>
2022-02-10 09:20:07 -0500

    Updated database

--- app.py
+++ app.py
@@ -7,10 +7,11 @@ from flask_sqlalchemy import SQLAlchemy
 
 app=Flask(__name__)
 CORS(app)
+app.secret_key = "secret"
 app.config['CORS_HEADERS']='Content-Type'
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://ronexffqrugnjx:5ab471aedc41d2a7a1f8da2a0cc912c5210dfe9ff83e2d3fb71bf4fa0692f17d@ec2-34-194-171-47.compute-1.amazonaws.com:5432/d77is03fnt6nrs"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-heroku=Heroku()
+heroku=Heroku(app)
 
 
 db=SQLAlchemy(app)
