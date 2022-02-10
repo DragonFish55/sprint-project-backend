b2286597a8e2b37763092b7d3cd100f3e7de9aaf
DragonFish55 <61723984+DragonFish55@users.noreply.github.com>
2022-02-09 23:01:33 -0500

    Updated database

--- app.py
+++ app.py
@@ -5,10 +5,7 @@ from models import *
 import os
 
 
-
-dbname="postgresql-rectangular-42071"
 app=Flask(__name__)
-
 CORS(app)
 app.config['CORS_HEADERS']='Content-Type'
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://ronexffqrugnjx:5ab471aedc41d2a7a1f8da2a0cc912c5210dfe9ff83e2d3fb71bf4fa0692f17d@ec2-34-194-171-47.compute-1.amazonaws.com:5432/d77is03fnt6nrs"
@@ -18,8 +15,7 @@ heroku=Heroku()
 #migrate=Migrate()
 db.init_app(app)
 #migrate.init_app(app,db)
-with app.app_context():
-    db.create_all()
+db.create_all()
 
 #create local account
 @app.route('/signup', methods = ["POST"])
