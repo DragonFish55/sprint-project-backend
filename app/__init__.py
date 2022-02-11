500c279a4b943c74610888ed80f04cdea2026251
DragonFish55 <61723984+DragonFish55@users.noreply.github.com>
2022-02-10 23:48:03 -0500

    Updated db

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,13 @@
-from flask import Flask, Response, request
+from flask import Flask, Response, request, jsonify
 from flask_cors import CORS, cross_origin
 from flask_heroku import Heroku
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from models import User
 
 app=Flask(__name__)
-
+CORS(app)
+app.config['CORS_HEADERS']='Content-Type'
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql+psycopg2://ronexffqrugnjx:5ab471aedc41d2a7a1f8da2a0cc912c5210dfe9ff83e2d3fb71bf4fa0692f17d@ec2-34-194-171-47.compute-1.amazonaws.com:5432/d77is03fnt6nrs"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
@@ -14,6 +16,48 @@ migrate = Migrate(app,db)
 app.secret_key = "secret"
 heroku=Heroku(app)
 db.init_app(app)
+
+#create local account
+@app.route('/api/signup', methods = ["POST"])
+@cross_origin
+def signup():
+    data_in = jsonify({'data':'nane'})
+    data_in.headers.add('Access-Control-Allow-Origin', '*')
+    data_in.headers.add('Access-Control-Allow-Headers', '*')
+    data_in.headers.add('Access-Control-Allow-Methods', '*')
+    print(data_in)
+    '''
+    user = data_in["username"]
+    confirm_pass = data_in['confirmpass']
+    password = data_in['password']
+    data_out = "false"
+    if(confirm_pass == password):
+        userlist = User.query.filter_by(username=user)
+        print(userlist)
+        data_out = "true"
+    else:
+        data_out = "false"
+    
+    '''
+    #data_out = jsonify(data_out)
+    
+
+    return data_in
+
+#signin account
+@app.route('/api/signin', methods = ["POST"])
+@cross_origin
+def signin():
+    
+    data_in = request.get_json()
+    user = data_in['username']
+    password = data_in['password']
+    data_out = "true"
+    data_out.headers.add('Access-Control-Allow-Origin', '*')
+    data_out.headers.add('Access-Control-Allow-Headers', '*')
+    data_out.headers.add('Access-Control-Allow-Methods', '*')
+    return jsonify(data_out)
+
 
 with app.app_context():
     db.create_all()
--- app/views.py
+++ app/views.py
@@ -1,50 +1,6 @@
-from flask import Flask, Response, request, jsonify
-from flask_cors import CORS, cross_origin
-from app import app
-from models import User
-
-CORS(app)
-app.config['CORS_HEADERS']='Content-Type: text/plain'
 
 
 
-#create local account
-@app.route('/api/signup', methods = ["POST"])
-@cross_origin
-def signup():
-    data_in = jsonify({'data':'nane'})
-    data_in.headers.add('Access-Control-Allow-Origin', '*')
-    data_in.headers.add('Access-Control-Allow-Headers', '*')
-    data_in.headers.add('Access-Control-Allow-Methods', '*')
-    print(data_in)
-    '''
-    user = data_in["username"]
-    confirm_pass = data_in['confirmpass']
-    password = data_in['password']
-    data_out = "false"
-    if(confirm_pass == password):
-        userlist = User.query.filter_by(username=user)
-        print(userlist)
-        data_out = "true"
-    else:
-        data_out = "false"
-    
-    '''
-    #data_out = jsonify(data_out)
-    
 
-    return data_in
 
-#signin account
-@app.route('/api/signin', methods = ["POST"])
-@cross_origin
-def signin():
-    
-    data_in = request.get_json()
-    user = data_in['username']
-    password = data_in['password']
-    data_out = "true"
-    data_out.headers.add('Access-Control-Allow-Origin', '*')
-    data_out.headers.add('Access-Control-Allow-Headers', '*')
-    data_out.headers.add('Access-Control-Allow-Methods', '*')
-    return jsonify(data_out)
+
