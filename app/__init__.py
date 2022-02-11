98b51f52ec85a3c93b94a77b0876223f2ef4e59f
DragonFish55 <61723984+DragonFish55@users.noreply.github.com>
2022-02-10 23:54:09 -0500

    Updated db

--- app/__init__.py
+++ app/__init__.py
@@ -17,6 +17,17 @@ app.secret_key = "secret"
 heroku=Heroku(app)
 db.init_app(app)
 
+class User(db.Model):
+    __tablename__ = 'user'
+
+    _id = db.Column("id",db.Integer, primary_key=True)
+    username = db.Column(db.String(30), unique=True, nullable=False)
+    password = db.Column(db.String(300), nullable=False)
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
 #create local account
 @app.route('/api/signup', methods = ["POST"])
 @cross_origin
