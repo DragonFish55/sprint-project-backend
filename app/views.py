ad690c9099a072acff900d8b61a5bd821557e47b
DragonFish55 <61723984+DragonFish55@users.noreply.github.com>
2022-02-10 19:24:17 -0500

    Updated files

--- app/views.py
+++ app/views.py
@@ -1,3 +1,39 @@
 from flask import Flask, Response, request, jsonify
 from flask_cors import CORS, cross_origin
 from app import app
+from models import User
+
+
+cors = CORS(app, resources={r"/signup":{"origins":"*"}})
+app.config['CORS_HEADERS']='Content-Type'
+
+#create local account
+@app.route('/signup', methods = ["POST"])
+@cross_origin()
+def signup():
+    data_in = request.get_json()
+    user = data_in["username"]
+    confirm_pass = data_in['confirmpass']
+    password = data_in['password']
+    data_out = "false"
+    if(confirm_pass == password):
+        userlist = User.query.filter_by(username=user)
+        print(userlist)
+        data_out = "true"
+    else:
+        data_out = "false"
+    data_out = jsonify(data_out)
+    data_out.headers.add('Access-Control-Allow-Origin', '*')
+    return data_out
+
+#signin account
+@app.route('/signin', methods = ["POST"])
+@cross_origin()
+def signin():
+    
+    data_in = request.get_json()
+    user = data_in['username']
+    password = data_in['password']
+    data_out = "true"
+    data_out.headers.add('Access-Control-Allow-Origin', '*')
+    return jsonify(data_out)
