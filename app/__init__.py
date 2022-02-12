3e2c0b611827406f681a3d9e1ecc3bd63a3ba0d6
DragonFish55 <61723984+DragonFish55@users.noreply.github.com>
2022-02-12 17:32:01 -0500

    Updated for cors

--- app/__init__.py
+++ app/__init__.py
@@ -8,8 +8,8 @@ app=Flask(__name__)
 CORS(app)
 app.config['CORS_HEADERS']='Content-Type'
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql+psycopg2://MainUserNew:happysquash@localhost:5432/UserApp"
-# app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql+psycopg2://ronexffqrugnjx:5ab471aedc41d2a7a1f8da2a0cc912c5210dfe9ff83e2d3fb71bf4fa0692f17d@ec2-34-194-171-47.compute-1.amazonaws.com:5432/d77is03fnt6nrs"
+#app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql+psycopg2://MainUserNew:happysquash@localhost:5432/UserApp"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql+psycopg2://ronexffqrugnjx:5ab471aedc41d2a7a1f8da2a0cc912c5210dfe9ff83e2d3fb71bf4fa0692f17d@ec2-34-194-171-47.compute-1.amazonaws.com:5432/d77is03fnt6nrs"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
