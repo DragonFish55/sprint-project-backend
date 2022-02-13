6892e7ac6d08a1559e0a47c56c173129c51fb649
DragonFish55 <61723984+DragonFish55@users.noreply.github.com>
2022-02-13 01:03:18 -0500

    Updated Signin and Signup Pages

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,6 @@
-from flask import Flask, Response, request,jsonify
+from __future__ import unicode_literals
+from encodings import utf_8
+from flask import Flask, Response, request,jsonify, session
 from flask_cors import CORS, cross_origin
 from flask_heroku import Heroku
 from flask_sqlalchemy import SQLAlchemy
@@ -13,18 +15,14 @@ app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql+psycopg2://MainUserNew:happy
 #app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql+psycopg2://ronexffqrugnjx:5ab471aedc41d2a7a1f8da2a0cc912c5210dfe9ff83e2d3fb71bf4fa0692f17d@ec2-34-194-171-47.compute-1.amazonaws.com:5432/d77is03fnt6nrs"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-
-
 db=SQLAlchemy(app)
 migrate = Migrate(app,db)
 app.secret_key = "secret"
 heroku=Heroku(app)
 db.init_app(app)
 
-
-
-class User2(db.Model):
-    __tablename__ = 'user2'
+class User(db.Model):
+    __tablename__ = 'user'
 
     _id = db.Column("id",db.Integer, primary_key=True)
     username = db.Column(db.String(30), nullable=False)
@@ -37,61 +35,69 @@ class User2(db.Model):
         self.password = password
         self.enc_key = enc_key
         
+class Session(db.Model):
+    __tablename__ = 'session'
 
-#create local account
+    _id = db.Column("id",db.Integer, db.ForeignKey("user.id"))
+    sessionid = db.Column(db.String(30), nullable=False, primary_key=True)
+    
+    def __init__(self, sessionid):
+        self.sessionid = sessionid
+
+#create account
 @app.route('/api/signup', methods = ["POST"])
 @cross_origin()
 def signup():
     data_in = request.get_json()
-    print(data_in)
-    
     user = data_in["username"]
     confirm_pass = data_in['confirmpass']
     password = data_in['password']
     data_out = "false"
     if(confirm_pass == password):
-        find_user = User2.query.filter_by(username=user).first()
+        find_user = User.query.filter_by(username=user).first()
         if(find_user is None):
             enc_key = Fernet.generate_key()
             enc_key_dec = enc_key.decode()
             fernet_var = Fernet(enc_key)
             encrypted_pass = fernet_var.encrypt(password.encode())
-            new_user = User2(username=user,password=encrypted_pass,enc_key=enc_key_dec)
+            firstpass = encrypted_pass.decode()
+            new_user = User(username=user,password=firstpass,enc_key=enc_key_dec)
             db.session.add(new_user)
             db.session.commit()
+            data_out = "true"
         else:
             data_out = "false"
     else:
         data_out = "false"
-    
-    data_out = jsonify({"hi":"thre"})
-    
-
     return data_out
 
 #signin account
 @app.route('/api/signin', methods = ["POST"])
 @cross_origin()
 def signin():
+    data_out = ''
     decrypt_pass = ''
     data_in = request.get_json()
     user = data_in['username']
     password = data_in['password']
-    print(password)
-
-    find_user = User2.query.filter_by(username=user).first()
-    print('hi')
+    find_user = User.query.filter_by(username=user).first()
     if(find_user is not None):
         enc_key = find_user.enc_key
-        print('there')
-        saved_pass = find_user.password
-        print(enc_key)
+        enc_key = enc_key.encode()
         fernet_var = Fernet(enc_key)
-        print('bye')
-        decrypt_pass = fernet_var.decrypt(saved_pass)
-        print('fly')
-        if(password == decrypt_pass):
-            print("they are equal")
+        saved_pass = find_user.password
+        encrypt_pass = fernet_var.decrypt(saved_pass.encode()).decode()
+        if(password == encrypt_pass):
+            session = Session.query.filter_by(id=find_user.id)
+            if(session is not None):
+                #session.sessionid = 
+                #print("they are equal")
+                #find_user.sessionid = 
+                data_out = "true"
+            else:
+                data_out = 'false'
+        else:
+            data_out = "false"
     print(decrypt_pass)
     print(password)
     #data_out.headers.add('Access-Control-Allow-Origin', '*')
